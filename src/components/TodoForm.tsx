e249412444de4e5751426018d256b93a843dedb6
devandtravel <devandtravel@gmail.com>
2021-07-01 13:25:35 +0300

    edit: useRef instead of useState in TodoForm input

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 
 export const TodoForm: React.FC = () => {
-    const [title, setTitle] = useState<string>('')
-    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.target.value)
-    }
+    const ref = useRef<HTMLInputElement>(null)
+
     const keyPressHandler = (event: React.KeyboardEvent) => {
-        event.key === 'Enter' && console.log(title)
-        setTitle('')
+        if (event.key === 'Enter') {
+            console.log(ref.current!.value)
+            ref.current!.value = ''
+        }
     }
+
     return (
         <div className='input-field mt2'>
             <input
-                onChange={changeHandler}
                 onKeyPress={keyPressHandler}
-                value={title}
+                ref={ref}
                 type='text'
                 id='title'
                 placeholder='Activity title'
